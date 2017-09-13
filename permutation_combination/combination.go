68e92cf2262aa1a43c9aac939bda38784e0fee2b
blurtheart <fuhongbofhb@163.com>
2017-09-13 19:43:05 +0800

    permutation and combine multi strings

---
+++ permutation_combination/combination.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+//
+func doCombine(strs []string, str string) []string {
+	if len(strs) == 0 {
+		ret := make([]string, len(str))
+		for i := 0; i < len(str); i++ {
+			ret[i] = string(str[i])
+		}
+		return ret
+	}
+
+	ret := make([]string, len(strs)*len(str))
+	for i := 0; i < len(strs); i++ {
+		for j := 0; j < len(str); j++ {
+			ret[i*len(str)+j] = strs[i] + string(str[j])
+		}
+	}
+	return ret
+}
+
+// 对多个数组进行排列组合
+// input: a:"abc"	b "def"
+// output: []string{"ad", "ae", "af", "bd", "be", "bf"}
+func combineString(strs ...string) []string {
+	var ret []string
+	for _, str := range strs {
+		ret = doCombine(ret, str)
+	}
+	return ret
+}
+
+func main() {
+	a, b, c := "abc", "def", "ghi"
+	fmt.Println(combineString(a, b, c))
+}
