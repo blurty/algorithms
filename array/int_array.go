b7e5eaecdfb1410e98c50f49b394e5d3373c1619
blurtheart <fuhongbofhb@163.com>
2018-01-30 08:02:45 +0800

    merge two sorted integer arrays

--- array/int_array.go
+++ array/int_array.go
@@ -91,3 +91,25 @@ func InsertInterval(intervals []Interval, newInterval Interval) []Interval {
 	}
 	return ret
 }
+
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+// nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+// The number of elements initialized in nums1 and nums2 are m and n respectively.
+func MergeSortedArrays(nums1 []int, m int, nums2 []int, n int) {
+	tail := m + n - 1
+	i, j := m-1, n-1
+	for i >= 0 && j >= 0 {
+		if nums1[i] < nums2[j] {
+			nums1[tail] = nums2[j]
+			j--
+		} else {
+			nums1[tail] = nums1[i]
+			i--
+		}
+		tail--
+	}
+	for ; j >= 0; j-- {
+		nums1[tail] = nums2[j]
+		tail--
+	}
+}
