984782c624d064db2ea5789a7800c697a22d3bf3
blurtheart <fuhongbofhb@163.com>
2019-09-08 20:45:31 +0800

    add word break2

--- strings/word_break.go
+++ strings/word_break.go
@@ -33,18 +33,48 @@ func inSliceString(s string, ss []string) bool {
 	return false
 }
 
-func findAllSubStringIndex(s string, sub string) (result []int) {
+/*
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+add spaces in s to construct a sentence where each word is a valid dictionary word. Return
+all such possible sentences.
+Note:
+- The same word in the dictionary may be reused multiple times in the segmentation.
+- You may assume the dictionary does not contain duplicate words.
+*/
+func WordBreak2(s string, wordDict []string) []string {
+	results := make([]string, 0)
+	for i := 0; i <= len(s); i++ {
+		if inSliceString(s[:i], wordDict) {
+			if i == len(s) {
+				results = append(results, s)
+				continue
+			}
+			subResults := WordBreak2(s[i:], wordDict)
+			for _, subResult := range subResults {
+				results = append(results, s[:i]+" "+subResult)
+			}
+		}
+	}
+	return results
+}
+
+func findAllSubStringIndex(s string, sub string) []int {
 	i := 0
 	cs := s
+	result := make([]int, 0)
 	for i < len(s) {
 		cs = cs[i:]
 		idx := ostrings.Index(cs, sub)
 		if idx != -1 {
-			result = append(result, idx)
+			num := idx
+			if len(result) > 0 {
+				num += result[len(result)-1] + len(sub)
+			}
+			result = append(result, num)
 			i = idx + len(sub)
 		} else {
 			break
 		}
 	}
-	return
+	return result
 }
--- strings/work_break_test.go
+++ strings/work_break_test.go
@@ -1,30 +1,31 @@
 package strings
 
 import (
-	"testing"
 	"reflect"
+	"sort"
+	"testing"
 )
 
 func TestFindAllSubStringIndex(t *testing.T) {
 	testCases := []struct {
-		s string
-		sub string
+		s       string
+		sub     string
 		indexes []int
-	} {
+	}{
 		{
-			s:"applepenapple",
-			sub:"apple",
-			indexes:[]int{0, 8},
+			s:       "applepenapple",
+			sub:     "apple",
+			indexes: []int{0, 8},
 		},
 		{
-			s:"apple",
-			sub:"cat",
-			indexes:[]int{},
+			s:       "apple",
+			sub:     "cat",
+			indexes: []int{},
 		},
 	}
 	for _, testCase := range testCases {
 		gotResult := findAllSubStringIndex(testCase.s, testCase.sub)
-		if reflect.DeepEqual(testCase.indexes, gotResult) {
+		if !reflect.DeepEqual(testCase.indexes, gotResult) {
 			t.Errorf("findAllSubStringIndex(%v,%v) want %v, got %v", testCase.s, testCase.sub, testCase.indexes, gotResult)
 		}
 	}
@@ -32,35 +33,77 @@ func TestFindAllSubStringIndex(t *testing.T) {
 
 func TestWordBreak(t *testing.T) {
 	testCases := []struct {
-		s string
-		d []string
+		s          string
+		d          []string
 		wantResult bool
-	} {
+	}{
 		{
-			s:"applepenapple",
-			d:[]string{"apple", "pen"},
-			wantResult:true,
+			s:          "applepenapple",
+			d:          []string{"apple", "pen"},
+			wantResult: true,
 		},
 		{
-			s:"leetcode",
-			d:[]string{"leet", "code"},
-			wantResult:true,
+			s:          "leetcode",
+			d:          []string{"leet", "code"},
+			wantResult: true,
 		},
 		{
-			s:"catsandog",
-			d:[]string{"cats", "dog", "sand", "and", "cat"},
-			wantResult:false,
+			s:          "catsandog",
+			d:          []string{"cats", "dog", "sand", "and", "cat"},
+			wantResult: false,
 		},
 		{
-			s:"catnamecat",
-			d:[]string{"cat", "catname"},
-			wantResult:true,
+			s:          "catnamecat",
+			d:          []string{"cat", "catname"},
+			wantResult: true,
 		},
 	}
 	for _, testCase := range testCases {
 		gotResult := WordBreak(testCase.s, testCase.d)
 		if gotResult != testCase.wantResult {
 			t.Errorf("WordBreak(%v,%v) want %v, got %v", testCase.s, testCase.d, testCase.wantResult, gotResult)
+		}
+	}
+}
+
+func TestWordBreak2(t *testing.T) {
+	testCases := []struct {
+		s          string
+		d          []string
+		wantResult []string
+	}{
+		{
+			s:          "applepenapple",
+			d:          []string{"apple", "pen"},
+			wantResult: []string{"apple pen apple"},
+		},
+		{
+			s:          "leetcode",
+			d:          []string{"leet", "code"},
+			wantResult: []string{"leet code"},
+		},
+		{
+			s:          "catsandog",
+			d:          []string{"cats", "dog", "sand", "and", "cat"},
+			wantResult: []string{},
+		},
+		{
+			s:          "catnamecat",
+			d:          []string{"cat", "catname", "name"},
+			wantResult: []string{"catname cat", "cat name cat"},
+		},
+		{
+			s:          "catsanddog",
+			d:          []string{"cat", "cats", "and", "sand", "dog"},
+			wantResult: []string{"cat sand dog", "cats and dog"},
+		},
+	}
+	for _, testCase := range testCases {
+		gotResult := WordBreak2(testCase.s, testCase.d)
+		sort.Strings(gotResult)
+		sort.Strings(testCase.wantResult)
+		if !reflect.DeepEqual(gotResult, testCase.wantResult) {
+			t.Errorf("WordBreak2(%v,%v) want %v, got %v", testCase.s, testCase.d, testCase.wantResult, gotResult)
 		}
 	}
 }
